fb959ef8018cac1d075a4350ed9231bff2e85a69
Коваленко Роман Александрович <r.kovalenko@bftcom.com>
2023-12-25 17:47:25 +0300

    modified finalWorks

--- FinalWork.cs
+++ FinalWork.cs
@@ -7,22 +7,15 @@ namespace final
 {
     public class FinalWork
     {
-        private const int MAX_LENGTH = 3;
+        private const int ELEMENT_MAX_LENGTH = 3;
         public static void Main(string[] args) {
             string[] arr = {"Hello", "2", "world", ":-)"};
 
-            Console.WriteLine("[\"{0}\"]", string.Join("\", \"", GetArrayWithRestrictElementLength(arr, MAX_LENGTH)));
+            Console.WriteLine("[\"{0}\"]", string.Join("\", \"", GetArrayWithRestrictElementLength(arr, ELEMENT_MAX_LENGTH)));
         }
 
         public static string[] GetArrayWithRestrictElementLength(string[] array, int maxLength) {
-            int newLength = 0;
-            foreach(string el in array) {
-                if(el.Length <= maxLength) {
-                    newLength++;
-                }
-            }
-
-            string[] newArray = new string[newLength];
+            string[] newArray = new string[GetLengthForArray(array, maxLength)];
             int index = 0;
             for(int i = 0; i < array.Length; i++) {        
                 if(array[i].Length <= maxLength) {
@@ -30,6 +23,16 @@ namespace final
                 }
             }
             return newArray;
+        }
+
+        private static int GetLengthForArray(string[] array, int maxLength) {
+            int newLength = 0;
+            foreach(string el in array) {
+                if(el.Length <= maxLength) {
+                    newLength++;
+                }
+            }
+            return newLength;
         }
     }
 }
