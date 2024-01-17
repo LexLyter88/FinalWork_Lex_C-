eb41490211d0db1b08ddce2e5f8307af0afb247d
Aleksey <yashnikov88@mail.ru>
2024-01-17 15:08:07 +0300

    Final Homework

--- FinalWork.cs
+++ FinalWork.cs
@@ -8,7 +8,8 @@ namespace final
     public class FinalWork
     {
         private const int ELEMENT_MAX_LENGTH = 3;
-        public static void Main(string[] args) {
+        public static void Main(string[] args) 
+        {
             string[] arr = {"Hello", "2", "world", ":-)"};
 
             Console.WriteLine("[\"{0}\"]", string.Join("\", \"", GetArrayWithRestrictElementLength(arr, ELEMENT_MAX_LENGTH)));
@@ -17,8 +18,10 @@ namespace final
         public static string[] GetArrayWithRestrictElementLength(string[] array, int maxLengthElement) {
             string[] newArray = new string[GetLengthForArray(array, maxLengthElement)];
             int index = 0;
-            for(int i = 0; i < array.Length; i++) {        
-                if(array[i].Length <= maxLengthElement) {
+            for(int i = 0; i < array.Length; i++) 
+            {        
+                if(array[i].Length <= maxLengthElement) 
+                {
                     newArray[index++] = array[i];
                 }
             }
@@ -27,8 +30,10 @@ namespace final
 
         private static int GetLengthForArray(string[] array, int maxLengthElement) {
             int newLength = 0;
-            foreach(string el in array) {
-                if(el.Length <= maxLengthElement) {
+            foreach(string el in array) 
+            {
+                if(el.Length <= maxLengthElement) 
+                {
                     newLength++;
                 }
             }
